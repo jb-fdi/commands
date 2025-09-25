c47a87b31c444d256966855962e5e40bf3c07f40
jb-fdi <70650130+jb-fdi@users.noreply.github.com>
2025-09-25 11:44:53 -0400

    Update BitdefenderInstaller.ps1

--- powershell/BitdefenderInstaller.ps1
+++ powershell/BitdefenderInstaller.ps1
@@ -1,13 +1,16 @@
-$installerurl = read-host "enter the installer url"
+$installerurl = $args[0]
 
 if($installerurl -match "https://cloud.gravityzone.bitdefender.com"){
     new-item C:\utility -ItemType Directory -ErrorAction SilentlyContinue
     $path = 'c:\utility\'
     $fn = ($installerurl -split "/")[-1]
 
+
     invoke-restmethod $installerurl -OutFile ($path + 'temp.exe')
 
     Rename-Item -Path ($path + 'temp.exe') -NewName $fn
 
     start-process ($path + $fn) -argumentlist '/bdparams /silent'
+}else{
+   write-host "No/Invalid Bitdefender URL provided"
 }
